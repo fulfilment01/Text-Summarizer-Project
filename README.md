15ca4f816bdfdea46604649021158af87c73af54
fulfilment01 <tobiowolabi283@gmail.com>
2025-01-28 12:23:23 +0100

    model evaluation completed

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 6. update pipeline
 7. update the main.py
 8. update the app.py
+
+
+## My project environment is textS
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -23,3 +23,30 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
+
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
+    num_train_epochs: int
+    warmup_steps: int
+    per_devise_train_batch_size: int
+    weight_decay: float
+    loading_steps: int
+    evaluation_strategy: str
+    eval_steps: int
+    save_steps: float
+    gradient_accumulation_steps: int
+
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
